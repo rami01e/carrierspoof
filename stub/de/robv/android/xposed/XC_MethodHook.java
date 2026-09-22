78a8045bb362fde53d6c59be2241376e4df703b8
Rami <138402961+rami01e@users.noreply.github.com>
2026-09-22 22:55:27 +0300

    Add XC_MethodHook class for method hooking

---
+++ stub/de/robv/android/xposed/XC_MethodHook.java
@@ -0,0 +1,25 @@
+package de.robv.android.xposed;
+
+import java.lang.reflect.Member;
+
+public abstract class XC_MethodHook {
+    public XC_MethodHook() { }
+    protected void beforeHookedMethod(MethodHookParam param) throws Throwable { }
+    protected void afterHookedMethod(MethodHookParam param) throws Throwable { }
+
+    public static class MethodHookParam {
+        public Member method;
+        public Object thisObject;
+        public Object[] args;
+        private Object result = null;
+        private Throwable throwable = null;
+        public Object getResult() { return result; }
+        public void setResult(Object result) { this.result = result; }
+        public Throwable getThrowable() { return throwable; }
+        public void setThrowable(Throwable throwable) { this.throwable = throwable; }
+    }
+
+    public static class Unhook {
+        public void unhook() { }
+    }
+}
